fc3b79d2099e6e1d4d13010bb5fe438e5ede7636
Arseny Yankovsky <contact@arseny.dev>
2021-02-02 19:01:24 +0100

    fix(nestjs): Fixed NestJS wrapper failing on empty controllers/services (#13)
    * fix(nestjs): Fixed NestJS wrapper failing on empty controllers/services
    
    * fix(nestjs): Fixed NestJS wrapper failing on empty controllers/services

--- src/wrappers/nestjs.ts
+++ src/wrappers/nestjs.ts
@@ -9,12 +9,12 @@ import { wrapClass } from './common'
  * @returns {Function} NestJS module
  */
 export const wrapNestJsModule = (module: any) => {
-  Reflect.getMetadata('controllers', module).forEach((injectable) => {
+  Reflect.getMetadata('controllers', module)?.forEach((injectable) => {
     wrapClass('', injectable.name, injectable, true)
   })
 
-  Reflect.getMetadata('providers', module).forEach((injectable) =>
-    wrapClass('', injectable.name, injectable, true),
+  Reflect.getMetadata('providers', module)?.forEach((injectable) =>
+    wrapClass('', injectable.name, injectable, true)
   )
 
   return module
