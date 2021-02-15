4eec3060bc1e46ba18d3e11441cd882621808c16
ayankovsky <contact@arseny.dev>
2021-02-15 18:12:39 +0100

    feat(tracing): Improved express tracing: fixed a bug in the request body and added an option to filter requests

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { isFunction } from 'lodash-es'
+import { isFunction, isNull, isUndefined, isString } from 'lodash-es'
 import { config } from './config'
 
 export const isPromise = (value: any) =>
@@ -17,4 +17,25 @@ export const appendBodyChunk = (chunk, body) => {
     return body + chunk
   }
   return body
+}
+
+export const limitPayload = (
+  payload: object | string | undefined | null,
+  limit: number = config.maxPayloadLength,
+): object | string | undefined | null => {
+  if (isNull(payload) || isUndefined(payload)) {
+    return payload
+  }
+
+  if (isString(payload) && payload.length > limit) {
+    return `${payload.substr(0, limit - 3)}...`
+  }
+
+  const stringifiedPayload = JSON.stringify(payload)
+
+  if (stringifiedPayload.length > limit) {
+    return `${stringifiedPayload.substr(0, limit - 3)}...`
+  }
+
+  return payload
 }
