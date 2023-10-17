2bdf4e713bf3a2c62e23d634f5cd7fc2a71e647c
arsenyyankovski <arseny.yankovsky@emarketeer.com>
2023-10-17 15:34:39 +0200

    fix(tracer): fix error serialization in logs

--- src/module-trackers/utils.ts
+++ src/module-trackers/utils.ts
@@ -59,15 +59,12 @@ tryRequire.lastError = () => lastError
 
 export const getModules = function getModules(id: string) {
   const modules: any[] = []
-  console.log('getModules', id)
-  console.log('getModules', typeof require.resolve.paths)
   if (typeof require.resolve.paths !== 'function') {
     const module = tryRequire(id)
 
     if (module) {
       modules.push(module)
     }
-    console.log('modules found for id ', id, modules)
     return modules
   }
 
