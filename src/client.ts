dd8ea0b1b61ebd320ceb5cb390b1c72e26a8f4e3
arsenyyankovski <arseny.yankovsky@emarketeer.com>
2024-10-23 14:59:21 +0200

    feat(tracking): move to a newer model of tracking via esbuild module wrapping

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,6 @@
 import awsSdkV3Tracker from "./module-trackers/aws-sdk-v3"
+import {trackHttp} from "./module-trackers/http/http"
+import {trackHttp2} from "./module-trackers/http/http2"
 
 export * from './tracer'
 export * from './wrappers'
@@ -6,3 +8,5 @@ export * from './entities'
 export * from './module-trackers'
 
 awsSdkV3Tracker.init()
+trackHttp()
+trackHttp2()
